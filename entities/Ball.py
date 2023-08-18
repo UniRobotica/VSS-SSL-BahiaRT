d98553a7c0c20a9cfad5fbfaaa57770ac87ed1b6
Luis <magalhaes4010cms@gmail.com>
2023-08-18 15:01:13 -0300

    created speed module for Ball

--- entities/Ball.py
+++ entities/Ball.py
@@ -5,6 +5,9 @@ Created by: - Luís Henrique
 Date: 15/06/2023
 '''
 
+from collections import deque
+from utils import speed
+
 class Ball():
     """
     Contains the ball state
@@ -15,9 +18,26 @@ class Ball():
         env='simulation'
     ) -> None:
         
-        self.position = 0
-        self.velocity = 0
+        self.position = [.0,.0]
+        self.vx, self.vy = .0 , .0
+        
+        self.last_positions = {
+            'x': deque(maxlen=10),
+            'y': deque(maxlen=10),
+        }
+        
         self.env = env
+        
+    def _update_last_positions(self):
+        
+        self.last_positions['x'].append(self.position[0])
+        self.last_positions['y'].append(self.position[1])
+    
+    def _update_speed(self):
+        
+        self._update_last_positions()
+        self.vx = speed.calculate_linear_speed(self.last_positions['x'])
+        self.vy = speed.calculate_linear_speed(self.last_positions['y'])
         
     def update(self, frame) -> None:
         """
@@ -32,3 +52,5 @@ class Ball():
             ball_frame.get('x', 0),
             ball_frame.get('y', 0)
         ]
+        
+        self._update_speed()
